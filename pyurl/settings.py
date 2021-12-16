49df0b76bbad39b1be5ec9074c50b27865a89955
juanl <juan.levickas@gmail.com>
2021-12-16 17:41:00 -0300

    Hide secret key

--- pyurl/settings.py
+++ pyurl/settings.py
@@ -10,7 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+from secrets import *
 from pathlib import Path
+from . import secrets
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-nsw!*(3o0%#qx3ptbh*1av(&ov+^y__d&z7)!x8m+l$zz0b0!@'
+
+SECRET_KEY = secrets.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
