817942a5d03e957c05f138cd8a268f7fed9f3163
juanl <juan.levickas@gmail.com>
2021-12-23 00:33:54 -0300

    Added "Copy to clipboard" button, made some frontend adjustements

--- urlshortener/forms.py
+++ urlshortener/forms.py
@@ -4,4 +4,4 @@ from django.forms import TextInput
 
 class URLForm(forms.Form):
     url = forms.CharField(label='', max_length=100, widget=forms.TextInput(
-        attrs={'placeholder': 'Your URL', 'class': 'form-control', 'aria-describedby': 'button-addon1'}))
+        attrs={'placeholder': 'Your URL', 'autocomplete': 'off', 'class': 'form-control', 'aria-describedby': 'button-addon1'}))
--- urlshortener/static/urlshortener/js/main.js
+++ urlshortener/static/urlshortener/js/main.js
@@ -1,8 +1,19 @@
-console.log(document.querySelector('title').innerText)
-
 let input = document.querySelector('#id_url')
+let copybtn = document.querySelector('#success button')
 
 input.addEventListener('focus',(e)=>{
     console.log(input)
     input.value = ''
 })
+
+
+function updateClipboard(){
+    let url = document.querySelector('#shortURL').value
+    navigator.clipboard.writeText(url)
+    copybtn.innerHTML = 'Copied!'
+    setTimeout(function(){
+        copybtn.innerHTML = 'Copy'
+    },3000)
+}
+
+copybtn.addEventListener('click',updateClipboard)
